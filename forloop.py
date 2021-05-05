039a0e0010645dc7d1a9c45f35b883a453bc1594
GhaithAssaf9 <82778411+GhaithAssaf9@users.noreply.github.com>
2021-05-05 13:49:16 +0300

    ForLoop

---
+++ forloop.py
@@ -0,0 +1,38 @@
+#for i in range(0,151):
+   # print(i)
+   
+
+
+
+# for i in range(0,1001,5):
+#     print(i)
+
+
+
+
+# for i in range(1,101):
+#     if i%5==0:
+#         print("coding")
+#     elif i%10==0:
+#         print("coding Dojo") 
+   
+
+
+# sum=0
+# for i in range(0,500001,2):
+#     sum=sum+i
+# print(sum)
+
+
+
+# for i in range(2018,0,-4):
+#     print(i)
+
+
+# lownum=int(input("enter the first number"))
+# highnum=int(input("enter the second number"))
+# mult=int(input("enter the mult"))
+# for i in range(lownum,highnum+1):
+#     if i%mult==0:
+#         print(i)
+
