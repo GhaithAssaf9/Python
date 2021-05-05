3186354273a448f784c8a7ffea97bd403abd9e51
GhaithAssaf9 <82778411+GhaithAssaf9@users.noreply.github.com>
2021-05-05 14:39:17 +0300

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+# print("hello World")
+
+
+# x="rain"
+# print("hello",x)
+
+
+# x="rain"
+# print("hello"+x)
+
+# x=9
+# print("hello",x)
+
+
+# x=9
+# print("hello"+x)
+
+
+# x="orange"
+# y="banana"
+# print("i love to eat",x,"and",y)
+
+# x="orange"
+# y="banana"
+# print(f"i love to eat {x}.{y}")
