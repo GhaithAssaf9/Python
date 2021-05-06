fb14e9b1d3a86087f3c40a53b8a3e744daa91eaf
GhaithAssaf9 <82778411+GhaithAssaf9@users.noreply.github.com>
2021-05-06 06:32:27 +0300

    Function Basics 2

---
+++ Functions Basic II.py
@@ -0,0 +1,41 @@
+# def  codown(x):
+#     newList = []
+#     for i in range(x, -1, -1):
+#         newList.append(i)
+#     return newList
+# print(codown(5))
+
+
+# def printreturn(list):
+#     print(list[0])
+#     return list[1]
+# print (printreturn([5,12]))
+
+
+# def firstlength(list):
+#     print(list[0])
+#     return len(list)
+# print(firstlength([1,2,3]))
+
+
+# def values_greatersecond(list):
+#     if len(list)<2:
+#         return False
+#     newList = []
+#     for val in list:
+#         if val>list[1]:
+#             newList.append(val)
+#     print(len(newList))    
+#     return newList
+# print(values_greatersecond([12,4,7,8,9,1,14,4,16]))
+# print(values_greatersecond([12,0,7,8,9,1,14,4,16]))
+# print(values_greaterecond([1]))
+# print(values_greatersecond([]))
+
+
+# def length_and_value(size,value):
+#     newList = []
+#     for i in range(size):
+#         newList.append(value)
+#     return newList
+# print(length_and_value(2,9))
